4c5684fc54cbe3ceb36b8de2437e601225f45172
! NON <77342717+NONSISNON@users.noreply.github.com>
2021-02-24 12:17:27 -0600

    Capitulo de codigo 1
    Conectando el bot a discord

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //Aqui empezara el codigo del bot
+//Empieza el codigo
+const Discord = require('discord.js'); //Declaramos que la constante Discord
+va a requerir del modulo discord.js
+
+//Definimos al cliente
+const client = new Discord.Client();
+//Este sera nuestro bot 
+//Llamamos a la config
+const config = require('./config.json');
+
+client.login(config.token); //Llamamos la config y el apartado de token 
